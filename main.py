fdb436e44d38b77936b691dbf34f0ba307a5125e
sandeepse3 <sandeepse3@gmail.com>
2026-04-08 09:09:29 +0530

    while loop

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print('timeip')
+answer=''
+i=1
+while i<=3:
+    answer= input('do you agree(yes/no):')
+    if answer=='yes':
+        print('Glad we are on the same page')
+        break
+    i+=1
+else:
+    print('3 strikes,you are out')
+
